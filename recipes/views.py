6a37ad1555184faa01fbe8b95ad105f508fb75bd
Ben O'Neill <ben@oneill.sh>
2024-09-21 21:34:12 -0400

    More fixes

--- recipes/views.py
+++ recipes/views.py
@@ -10,27 +10,23 @@ def index(request):
     return render(request, "recipes/index.html", context)
 
 
-def available_ingredients(request, ingredient_id):
-    ingredient = get_object_or_404(Ingredient, pk=ingredient_id)
-    return render(request, "recipes/ingredient.html", {"ingredient": ingredient})
-
-def available_recipes(request):
+def available_recipes_view(request):
     context = {
         "recipes": available_recipes(request.user),
         "username": request.user.username
     }
     return render(request, "recipes/available_recipes.html", context)
 
-def available_recipes_with_ingredient(request, ingredient_id):
+def available_recipes_with_ingredient_view(request, ingredient_id):
     """
     Show recipes user has all ingredients for that includes ingredient_id
     """
     pass
 
-def ingredient(request, ingredient_id):
+def ingredient_view(request, ingredient_id):
     recipe = get_object_or_404(Ingredient, pk=ingredient.id)
     return render(request, "recipes/ingredient.html", {"ingredient": ingredient})
 
-def recipe(request, recipe_id):
+def recipe_view(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, "recipes/recipe.html", {"recipe": recipe})
