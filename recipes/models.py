d61b5708d9ceae1549b0b4ef3ef5924a9a28813d
Ben O'Neill <ben@oneill.sh>
2024-09-21 21:34:05 -0400

    Rework views

--- recipes/models.py
+++ recipes/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.conf import settings
 
 class Recipe(models.Model):
-    """
-    TODO
-    """
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=2000)
 
@@ -20,18 +17,15 @@ class Recipe(models.Model):
 
 
 class Ingredient(models.Model):
-    """
-    TODO
-    """
     name = models.CharField(max_length=100)
+
+    def user_can_make(self, user):
+        pass
 
     def __str__(self):
         return str(self.name)
 
 class RecipeIngredient(models.Model):
-    """
-    TODO
-    """
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
 
@@ -39,9 +33,6 @@ class RecipeIngredient(models.Model):
         return str(self.recipe.name + ": " + self.ingredient.name)
 
 class UserIngredient(models.Model):
-    """
-    TODO
-    """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Recipe
+from .models import Recipe, Ingredient
 from .logic import available_recipes
 
 def index(request):
@@ -9,12 +9,28 @@ def index(request):
     }
     return render(request, "recipes/index.html", context)
 
-def recipe_detail(request, recipe_id):
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, "recipes/recipe_detail.html", {"recipe": recipe})
 
-def user_ingredient_detail(request, ingredient_id):
+def available_ingredients(request, ingredient_id):
+    ingredient = get_object_or_404(Ingredient, pk=ingredient_id)
+    return render(request, "recipes/ingredient.html", {"ingredient": ingredient})
+
+def available_recipes(request):
+    context = {
+        "recipes": available_recipes(request.user),
+        "username": request.user.username
+    }
+    return render(request, "recipes/available_recipes.html", context)
+
+def available_recipes_with_ingredient(request, ingredient_id):
     """
     Show recipes user has all ingredients for that includes ingredient_id
     """
     pass
+
+def ingredient(request, ingredient_id):
+    recipe = get_object_or_404(Ingredient, pk=ingredient.id)
+    return render(request, "recipes/ingredient.html", {"ingredient": ingredient})
+
+def recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, "recipes/recipe.html", {"recipe": recipe})
