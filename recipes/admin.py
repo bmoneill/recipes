56cd8c21e6b14a30201eed8832d64d966521a974
Ben O'Neill <ben@oneill.sh>
2025-06-30 11:01:14 -0400

    Remove quantity model stuff

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 from recipes.models.price import Price
-from recipes.models.quantity import Quantity
 from .models.recipe import Recipe
 from .models.ingredient import Ingredient
 from .models.recipe_ingredient import RecipeIngredient
@@ -12,4 +11,3 @@ admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
 admin.site.register(UserIngredient)
 admin.site.register(Price)
-admin.site.register(Quantity)
--- recipes/models/__init__.py
+++ recipes/models/__init__.py
@@ -3,5 +3,4 @@ from .ingredient import Ingredient
 from .recipe_ingredient import RecipeIngredient
 from .user_ingredient import UserIngredient
 from .recipes_user import RecipesUser
-from .quantity import Quantity
 from .price import Price
