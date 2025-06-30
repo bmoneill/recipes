ec1bd941b0cb6bf2d9e922c078703321c4f201ab
Ben O'Neill <ben@oneill.sh>
2025-06-30 14:38:32 -0400

    Organize imports

--- cookingapi/serializers/__init__.py
+++ cookingapi/serializers/__init__.py
@@ -1,3 +1,3 @@
 from .user_serializer import UserSerializer
-from .recipe_serializer import RecipeSerializer
 from .ingredient_serializer import IngredientSerializer
+from .recipe_serializer import RecipeSerializer
