bfd7bf490ec59f5885a3c68601efe82332483e96
Ben O'Neill <ben@oneill.sh>
2025-06-30 09:52:42 -0400

    Price, Quantity on admin portal

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+
+from recipes.models.price import Price
+from recipes.models.quantity import Quantity
 from .models.recipe import Recipe
 from .models.ingredient import Ingredient
 from .models.recipe_ingredient import RecipeIngredient
@@ -8,3 +11,5 @@ admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
 admin.site.register(UserIngredient)
+admin.site.register(Price)
+admin.site.register(Quantity)
