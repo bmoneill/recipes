ceda0d7d29463c901932b459f180053e01e837b7
Ben O'Neill <ben@oneill.sh>
2025-06-30 09:32:31 -0400

    Document recipe_ingredient

--- recipes/models/recipe_ingredient.py
+++ recipes/models/recipe_ingredient.py
@@ -1,14 +1,22 @@
+"""
+This module defines the RecipeIngredient model for the recipes application.
+
+The RecipeIngredient model represents an ingredient used in a recipe with a
+specific quantity.
+"""
+
 from django.db import models
 
 class RecipeIngredient(models.Model):
+    """Model representing an ingredient used in a recipe with a specific quantity."""
+
     id = models.AutoField(primary_key=True, null=False)
     recipe = models.ForeignKey('recipes.Recipe', on_delete=models.CASCADE, null=False)
-    ingredient = models.ForeignKey('recipes.Ingredient', on_delete=models.CASCADE, null=False)
+    ingredient = models.ForeignKey('recipes.Ingredient', on_delete=models.CASCADE,
+                                               null=False)
     quantity = models.ForeignKey('recipes.Quantity', on_delete=models.CASCADE, null=True)
 
     def __str__(self) -> str:
-        """Returns a string representation of the RecipeIngredient instance.
-        """
         return f"{self.recipe.name}: {self.quantity.amount} {self.ingredient.name}"
 
     def __eq__(self, other):
