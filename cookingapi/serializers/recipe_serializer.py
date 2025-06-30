13820ae5e6c89fe9e24019931112e74e72cf5348
Ben O'Neill <ben@oneill.sh>
2025-06-30 14:38:23 -0400

    Simplify recipe serializer

--- cookingapi/serializers/recipe_serializer.py
+++ cookingapi/serializers/recipe_serializer.py
@@ -1,22 +1,17 @@
 from rest_framework import serializers
-from cookingapi.models import Recipe, RecipeIngredient
-from cookingapi.serializers.ingredient_serializer import IngredientSerializer
+from cookingapi.models import Recipe
+from . import IngredientSerializer
 
-class RecipeSerializer(serializers.ModelSerializer):
+class RecipeSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer for the Recipe model."""
 
     name = serializers.CharField(max_length=100)
     author = serializers.CharField(source='author.username', read_only=True)
     description = serializers.CharField(max_length=2000)
-    ingredients = serializers.SerializerMethodField()
+
+    ingredients = IngredientSerializer(many=True, read_only=True)
     time = serializers.DurationField(default=None, allow_null=True)
 
     class Meta:
         model = Recipe
         fields = ['name', 'author', 'description', 'ingredients', 'time']
-
-    def get_ingredients(self, obj):
-        """Returns a list of ingredients for the recipe."""
-        recipe_ingredients = RecipeIngredient.objects.filter(recipe=obj)
-        ingredients = [ri.ingredient for ri in recipe_ingredients]
-        return IngredientSerializer(ingredients, many=True).data
