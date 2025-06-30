89b3e5d9433b0da9b4057a198d1b0a2ec0937a0c
Ben O'Neill <ben@oneill.sh>
2025-06-30 12:46:35 -0400

    Show ingredients in recipeserializer

--- cookingapi/serializers/ingredient_serializer.py
+++ cookingapi/serializers/ingredient_serializer.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from cookingapi.models import Ingredient
 
 class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+    name = serializers.CharField(max_length=100, allow_blank=False)
+
     class Meta:
         model = Ingredient
         fields = ['id', 'name']
--- cookingapi/serializers/recipe_serializer.py
+++ cookingapi/serializers/recipe_serializer.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from cookingapi.models import Recipe, RecipeIngredient
+from cookingapi.serializers.ingredient_serializer import IngredientSerializer
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for the Recipe model."""
+
     name = serializers.CharField(max_length=100)
     author = serializers.CharField(source='author.username', read_only=True)
     description = serializers.CharField(max_length=2000)
@@ -13,7 +15,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['name', 'author', 'description', 'ingredients', 'time']
 
-
     def get_ingredients(self, obj):
-        """Return the ingredients associated with the recipe."""
-        return RecipeIngredient.objects.filter(recipe=obj).values('ingredient__id', 'ingredient__name')
+        """Returns a list of ingredients for the recipe."""
+        recipe_ingredients = RecipeIngredient.objects.filter(recipe=obj)
+        ingredients = [ri.ingredient for ri in recipe_ingredients]
+        return IngredientSerializer(ingredients, many=True).data
