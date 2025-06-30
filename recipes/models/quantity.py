a080b1aa91505aa56ec3367dfae3e80276509679
Ben O'Neill <ben@oneill.sh>
2025-06-30 08:55:12 -0400

    Document quantity

--- recipes/models/quantity.py
+++ recipes/models/quantity.py
@@ -1,6 +1,18 @@
 from django.db import models
+"""
+This module defines the Quantity model for the recipes application.
+It includes fields for the quantity amount and unit of measurement.
+
+Future enhancements could include adding support for different measurement
+units.
+
+Currently, the string representation of the Quantity model does not convert
+units for display in templates, which should be addressed in the future.
+"""
 
 class Quantity(models.Model):
+    """Model representing unit and quantity of an ingredient with a specific unit of measurement."""
+
     UNIT_CHOICES = {
         "tsp": "Teaspoon",
         "dsp": "Dessertspoon",
@@ -20,8 +32,8 @@ class Quantity(models.Model):
     unit = models.CharField(max_length=4, choices=list(UNIT_CHOICES.items()), null=False)
     amount = models.FloatField(null=False)
 
-    # TODO convert units when using this in templates
     def __str__(self):
+        # TODO convert units when using this in templates
         return f"{self.amount}{self.unit}"
 
     def __eq__(self, other):
